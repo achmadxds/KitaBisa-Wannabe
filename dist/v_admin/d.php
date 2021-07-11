77e89353cb9a00ac704c9b398f5e870516d8463f
Nur Achmad Shafy Raissa Tsani <75611252+achmadxds@users.noreply.github.com>
2021-07-11 21:04:02 +0700

    push

--- dist/v_admin/d.php
+++ dist/v_admin/d.php
@@ -1,53 +1,67 @@
 <?php
-    if(isset($_GET['kode'])){
-        $sql_cek = "SELECT * FROM mst_jenis WHERE id='".$_GET['kode']."'";
-        $query_cek = mysqli_query($con, $sql_cek);
-        $data_cek = mysqli_fetch_array($query_cek,MYSQLI_BOTH);
-    }
+if (isset($_GET['kode'])) {
+  $sql_cek = "SELECT * FROM mst_jenis WHERE id='" . $_GET['kode'] . "'";
+  $query_cek = mysqli_query($con, $sql_cek);
+  $data_cek = mysqli_fetch_array($query_cek, MYSQLI_BOTH);
+}
 ?>
-<div class="page-content">
-  <section class="row">
-    <div class="row">
-      <div class="col-12">
-        <div class="card">
-          <div class="card-header">
-            Master Jenis
-          </div>
-          <div class="card-body">
-            <form class="form-horizontal" action="#" method="post" enctype="multipart/form-data">
-              <div class="box-body">
-                <div class="box">
-                  <input type="hidden" class="form-control" name="txtId" oninvalid="InvalidMsg(this);"
-                    oninput="InvalidMsg(this);" value="<?php echo $data_cek['id']; ?>" required="">
+<div id="main">
+  <header class="mb-3">
+    <a href="#" class="burger-btn d-block d-xl-none">
+      <i class="bi bi-justify fs-3"></i>
+    </a>
+  </header>
 
-                  <div class="form-group ">
-                    <label class="col-sm-2 control-label">Nama jenis</label>
-                    <div class="col-sm-5">
-                      <input class="form-control" name="id_pengaduan" value="<?php echo $data_cek['nama']; ?>"
-                        readonly />
-                    </div>
-                  </div>
 
-                  <div class="form-group">
-                    <label class="col-sm-2 control-label">Pesan</label>
-                    <div class="col-sm-8">
-                      <input class="form-control" name="txtIsi" value="<?php echo $data_cek['jenis']; ?>" readonly />
-                    </div>
-                  </div>
+  <div class="page-content">
+    <section class>
+      <a data-bs-toggle="modal" data-bs-target="#myModal" class="btn btn-primary btn-sm">Tambah Jenis</a>
+  </div>
+  <br>
+  <div class="row">
+    <div class="col-12">
+      <div class="card">
+        <div class="card-header">
+          Master Jenis
+        </div>
+        <div class="card-body">
+          <table class="table table-striped" id="table1">
+            <thead>
+              <center>
+                <tr>
+                  <th>No</th>
+                  <th>Jenis</th>
+                  <th></th>
+                </tr>
+              </center>
+            </thead>
+            <tbody>
 
-                </div>
-                <div class="box-footer">
-                  <!-- <input type="submit" name="btnSimpan" value="Balas" class="btn btn-success"> -->
-                  <button type="submit" class="btn btn-primary" name="Ubah">Balas</button>
-                  <a href="?page=jenis" title="Kembali" class="btn btn-warning">Batal</a>
-                </div>
-              </div>
+              <?php
+              $sql_tampil = "SELECT * FROM mst_jenis";
+              $query_tampil = mysqli_query($con, $sql_tampil);
+              $no = 1;
+              while ($data = mysqli_fetch_array($query_tampil, MYSQLI_BOTH)) {
+              ?>
+                <tr>
+                  <td><?php echo $no; ?></td>
+                  <td><?php echo $data['nama']; ?></td>
+                  <td>
+                    <a href="?page=jnsUbah&kode=<?php echo $data['id']; ?>" class='btn btn-warning btn-sm'><i class="fa fa-edit"></i></a>
+                    <a href="?page=jnsAksi&kode=<?php echo $data['id']; ?>" onclick="return confirm('Hapus Jenis ini ?')" class='btn btn-danger btn-sm'><i class="fa fa-trash"></i></i></a>
+                  </td>
+                </tr>
+              <?php
+                $no++;
+              }
 
-            </form>
-          </div>
+              ?>
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
+  </div>
 </div>
 
 </section>
--- dist/v_admin/jenis/view.php
+++ dist/v_admin/jenis/view.php
@@ -1,68 +1,66 @@
-<?php	
-	 include_once("koneksi.php");
-    ?>
+<?php
+include_once("koneksi.php");
+?>
 <div id="main">
-    <header class="mb-3">
-        <a href="#" class="burger-btn d-block d-xl-none">
-            <i class="bi bi-justify fs-3"></i>
-        </a>
-    </header>
+	<header class="mb-3">
+		<a href="#" class="burger-btn d-block d-xl-none">
+			<i class="bi bi-justify fs-3"></i>
+		</a>
+	</header>
 
-    
-    <div class="page-content">
-        <section class>
-        <a data-bs-toggle="modal" data-bs-target="#myModal" class="btn btn-primary btn-sm">Tambah Jenis</a> </div>
-        <br>
-            <div class="row">
-                <div class="col-12">
-                    <div class="card">
-                        <div class="card-header">
-                            Master Jenis
-                        </div>
-                        <div class="card-body">
-                            <table class="table table-striped" id="table1">
-                                <thead>
-                                    <center>
-                                        <tr>
-                                            <th>No</th>
-                                            <th>Jenis</th>
-                                            <th></th>
-                                        </tr>
-                                    </center>
-                                </thead>
-                                <tbody>
 
-                                    <?php
-            $sql_tampil = "SELECT * FROM mst_jenis";
-            $query_tampil = mysqli_query($con, $sql_tampil);
-            $no=1;
-            while ($data = mysqli_fetch_array($query_tampil,MYSQLI_BOTH)) {
-        ?>
-                                    <tr>
-                                        <td><?php echo $no; ?></td>
-                                        <td><?php echo $data['nama']; ?></td>
-                                        <td>
-                                            <a href="?page=jnsUbah&kode=<?php echo $data['id']; ?>"
-                                                class='btn btn-warning btn-sm'><i class="fa fa-edit"></i></a>
-                                            <a href="?page=jnsAksi&kode=<?php echo $data['id']; ?>"
-                                                onclick="return confirm('Hapus Jenis ini ?')"
-                                                class='btn btn-danger btn-sm'><i class="fa fa-trash"></i></i></a>
-                                        </td>
-                                    </tr>
-                                    <?php
-            $no++;
-            }
-        
-        ?>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
-            </div>
-    </div>
+	<div class="page-content">
+		<section class>
+			<a data-bs-toggle="modal" data-bs-target="#myModal" class="btn btn-primary btn-sm">Tambah Jenis</a>
+	</div>
+	<br>
+	<div class="row">
+		<div class="col-12">
+			<div class="card">
+				<div class="card-header">
+					Master Jenis
+				</div>
+				<div class="card-body">
+					<table class="table table-striped" id="table1">
+						<thead>
+							<center>
+								<tr>
+									<th>No</th>
+									<th>Jenis</th>
+									<th></th>
+								</tr>
+							</center>
+						</thead>
+						<tbody>
 
-    </section>
+							<?php
+							$sql_tampil = "SELECT * FROM mst_jenis";
+							$query_tampil = mysqli_query($con, $sql_tampil);
+							$no = 1;
+							while ($data = mysqli_fetch_array($query_tampil, MYSQLI_BOTH)) {
+							?>
+								<tr>
+									<td><?php echo $no; ?></td>
+									<td><?php echo $data['nama']; ?></td>
+									<td>
+										<a href="?page=jnsUbah&kode=<?php echo $data['id']; ?>" class='btn btn-warning btn-sm'><i class="fa fa-edit"></i></a>
+										<a href="?page=jnsAksi&kode=<?php echo $data['id']; ?>" onclick="return confirm('Hapus Jenis ini ?')" class='btn btn-danger btn-sm'><i class="fa fa-trash"></i></i></a>
+									</td>
+								</tr>
+							<?php
+								$no++;
+							}
+
+							?>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+</section>
 </div>
 <div id="myModal" class="modal fade">
 	<div class="modal-dialog">
@@ -72,28 +70,27 @@
 				<h4 class="modal-title">Jenis</h4>
 			</div>
 			<div class="modal-body">
-                <form action="?page=jnsAksi" method="post" enctype="multipart/form-data">
-					
-                <div class="form-group">
-                    <label>Jenis Donasi</label>
-                    <input type="text" class="form-control" name="txtJenis"/>
-                </div>
-                <div class="form-group">
-                    <label> Jenis </label>
-                    <div>
-                        <select name="txtPil" class="form-control" required>
-                        <option value=""> - Jenis -</option>
-                        <option value="1">Lembaga</option>
-                        <option value="0">Perseorangan</option>
-                        </select>
-                    </div>
-                </div>
-              </div>
-                
-				<div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Batal</button>
-                <button type="submit" class="btn btn-primary" name="btnSimpan">Simpan</button>
-				</div>
-			</form>
-        </div>
+				<form action="?page=jnsAksi" method="post" enctype="multipart/form-data">
 
+					<div class="form-group">
+						<label>Jenis Donasi</label>
+						<input type="text" class="form-control" name="txtJenis" />
+					</div>
+					<div class="form-group">
+						<label> Jenis </label>
+						<div>
+							<select name="txtPil" class="form-control" required>
+								<option value=""> - Jenis -</option>
+								<option value="1">Lembaga</option>
+								<option value="0">Perseorangan</option>
+							</select>
+						</div>
+					</div>
+			</div>
+
+			<div class="modal-footer">
+				<button type="button" class="btn btn-default" data-dismiss="modal">Batal</button>
+				<button type="submit" class="btn btn-primary" name="btnSimpan">Simpan</button>
+			</div>
+			</form>
+		</div>
