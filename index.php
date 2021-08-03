72330fd8bae2ea5434525c99c644e3218d014aae
Nur Achmad Shafy Raissa Tsani <75611252+achmadxds@users.noreply.github.com>
2021-08-04 06:32:57 +0700

    Update index.php

--- index.php
+++ index.php
@@ -14,6 +14,8 @@
   <!-- Google fonts-->
   <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
   <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
+  <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+
   <!-- Core theme CSS (includes Bootstrap)-->
   <link href="css/styles.css" rel="stylesheet" />
 </head>
@@ -43,14 +45,14 @@
             <li class="nav-item mx-0 mx-lg-1" data-bs-toggle="dropdown"><a href="javascript:void(0)" class="nav-link py-3 px-0 px-lg-3 rounded">Program</a></li>
             <div class="dropdown-menu">
               <?php
-                include_once('koneksi.php');
-                $dt = SelectAlljenis();
+              include_once('koneksi.php');
+              $dt = SelectAlljenis();
 
-                foreach ($dt as $key => $value) {
-                  ?>
-                    <a class="dropdown-item" href="<?php echo "indexByJenis.php?kode=" . $value['id'] ?>"><?php echo $value['nama'] ?></a>  
-                  <?php
-                }
+              foreach ($dt as $key => $value) {
+              ?>
+                <a class="dropdown-item" href="<?php echo "indexByJenis.php?kode=" . $value['id'] ?>"><?php echo $value['nama'] ?></a>
+              <?php
+              }
               ?>
             </div>
           </div>
@@ -100,7 +102,7 @@
         foreach ($a as $key => $value) {
         ?>
           <div class="col-md-6 col-lg-4 mb-5">
-            <div class="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal1">
+            <div class="portfolio-item mx-auto" data-id="<?php echo $value['nmProgram'] . "~" . $value['keterangan'] . "~" . $value['donasi'] . "~" . $value['tgl_akhir'] ?>" onclick="clicked(this)" data-bs-toggle="modal" data-bs-target="#portfolioModal1">
               <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
                 <div class="portfolio-item-caption-content text-center text-white">
                   <h2><?php echo $value['nmProgram'] ?></h2>
@@ -165,28 +167,46 @@
 
     </div>
   </section>
-  
-  <div class="portfolio-modal modal fade" id="portfolioModal1" tabindex="-1" aria-labelledby="portfolioModal1" aria-hidden="true">
-    <div class="modal-dialog modal-xl">
+
+  <div class="modal fade" id="portfolioModal1" tabindex="-1" aria-labelledby="portfolioModal1" aria-hidden="true">
+    <div class="modal-dialog">
       <div class="modal-content">
-        <div class="modal-header border-0"><button class="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button></div>
         <div class="modal-body text-center pb-5">
           <div class="container">
             <div class="row justify-content-center">
-              <div class="col-lg-8">
+              <div class="col-lg-12">
                 <!-- Portfolio Modal - Title-->
-                <h2 class="portfolio-modal-title text-secondary text-uppercase mb-0">Log Cabin</h2>
+                <h2 class="portfolio-modal-title text-secondary pt-3 text-uppercase mb-0">Detail Program</h2>
                 <!-- Icon Divider-->
                 <div class="divider-custom">
                   <div class="divider-custom-line"></div>
                   <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
                   <div class="divider-custom-line"></div>
                 </div>
-                <!-- Portfolio Modal - Image-->
-                <img class="img-fluid rounded mb-5" src="assets/img/portfolio/cabin.png" alt="..." />
-                <!-- Portfolio Modal - Text-->
-                <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
-                <button class="btn btn-primary" href="#!" data-bs-dismiss="modal">
+                <div class="row pb-2">
+                  <div class="col-6">
+                    <label for=""><b>Nama Program</b></label>
+                    <textarea style="resize: none;" id="namaP" cols="30" rows="2" class="form-control" readonly></textarea>
+                    <br>
+                    <label for=""><b>Keterangan</b></label>
+                    <textarea style="resize: none;" id="ketP" cols="30" rows="2" class="form-control" readonly></textarea>
+                    <br>
+                    <label for=""><b>Tujuan Donasi</b></label>
+                    <input type="text" class="form-control" id="tujP" readonly />
+                  </div>
+                  <div class="col-6">
+                    <label for=""><b>Atas Nama</b></label>
+                    <!-- <input type="text" class="form-control" id="pimpP" readonly /> -->
+                    <textarea style="resize: none;" id="pimpP" cols="30" rows="2" class="form-control" readonly></textarea>
+                    <br>
+                    <label for=""><b>Tanggal Berakhir</b></label>
+                    <textarea style="resize: none;" id="tglendP" cols="30" rows="2" class="form-control" readonly></textarea>
+                    <br>
+                    <label for=""><b>Donasi Terkumpul</b></label>
+                    <input type="text" class="form-control" id="ntujP" readonly />
+                  </div>
+                </div>
+                <button class="btn btn-primary mt-3" href="#!" data-bs-dismiss="modal">
                   <i class="fas fa-times fa-fw"></i>
                   Close Window
                 </button>
@@ -239,4 +259,16 @@
 <script type="text/javascript">
   var ProgressBar = require('progressbar.js')
   var line = new ProgressBar.Line('#aaooo')
+
+  function clicked(param) {
+    let A = $(param).data('id')
+    const data = A.split('~')
+
+    $('#namaP').val(data[0])
+    $('#ketP').val(data[1])
+    $('#tujP').val('Rp. ' + data[2])
+    $('#pimpP').val()
+    $('#tglendP').val(data[3])
+    $('#ntujP').val()
+  }
 </script>
